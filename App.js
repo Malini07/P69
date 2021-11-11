a06a560ba3001c33f4a652d7c5e42b8a4ba7a874
Malini07 <73117135+Malini07@users.noreply.github.com>
2021-11-11 17:14:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import ScanScreen from "./Screens/ScanScreen";
+
+
+export default class App extends React.Component {
+  render(){
+  return(
+    <AppContainer/> 
+  );
+  }
+}
+const AppContainer= createAppContainer()
+
+
+
+
+
+
+
+
